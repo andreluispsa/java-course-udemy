0b0ab87ef9a08ac57b6d3184db75f2c1cfdcab4b
André Sá <andrepena.sa@gmail.com>
2024-01-25 16:08:51 -0300

    aulas de orientação a objeto - funcionário

---
+++ src/orientacaoObjetoFuncionario/entities/Employee.java
@@ -0,0 +1,22 @@
+package orientacaoObjetoFuncionario.entities;
+
+public class Employee {
+
+    public String name;
+    public double salary;
+    public double tax;
+
+    public double netSalary() {
+        return salary - tax;
+    }
+
+    public void increaseSalary(double percentage) {
+        salary += salary * (percentage/100);
+    }
+
+    public String toString() {
+        return name
+                + ", $"
+                + String.format("%.2f", netSalary());
+    }
+}
